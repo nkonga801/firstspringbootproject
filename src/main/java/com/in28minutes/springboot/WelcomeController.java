2849ce4b2bf14e329077530310263b839741283e
nkonga801 <45779566+nkonga801@users.noreply.github.com>
2019-01-21 14:14:27 -0500

    Update WelcomeController.java

--- src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -18,7 +18,7 @@ public class WelcomeController {
         }
 }
 
-
+// Spring to manage this bean and create an instance of this. 
 @Component
 class WelcomeService {
     public String retrieveWelcomeMessage() {
